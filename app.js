fdec9e68193ea3407e3f0c14adc7d352adfed0ad
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2022-06-22 15:57:27 +0200

    lab-ironbeers it3

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const hbs = require('hbs');
+const path = require('path');
+const PunkAPIWrapper = require('punkapi-javascript-wrapper');
+
+const app = express();
+const punkAPI = new PunkAPIWrapper();
+
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Register the location for handlebars partials here:
+
+// ...
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.send({ beersFromApi });
+    })
+    .catch(error => console.log(error));
+});
+// Add the route handlers here:
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.listen(3000, () => console.log('🏃‍ on port 3000'));
